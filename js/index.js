e75ab31d7409be31efe6fc28b640b758d223644b
ejonahadziu <30577990+ejonahadziu@users.noreply.github.com>
2017-09-11 18:38:49 -0400

    draft#5

--- js/index.js
+++ js/index.js
@@ -18,5 +18,47 @@ $(document).ready(function() {
 		}
 	});
 
+	var quotes = [ 
+		"With enough coffee anything is possible",
+		"A coffee a day keeps the grumpy way",
+		"I only drink coffee on days ending in 'Y' ",
+	];
+
+	var i = 0;
+
+	function previousQuote() {
+		i--;
+		if (i === -1) {
+			i = quotes.length - 1;
+		}
+		changeQuote();
+	}
+
+	function nextQuote(){
+		i++;
+		if (i === quotes.length) {
+			i = 0;
+		}
+		changeQuote();
+}
+
+	function changeQuote(){
+		$("#quote").text(quotes[i]); 
+	}
+
+	$("#next").on("click", nextQuote);
+	$("#prev").on("click", previousQuote);
+	
+	$('.quote').carousel({
+  		interval: 2000
+	})
+
+	$("#location").click(function() { 
+
+    	$("#googleMap").show(); 
+
+    });
+
+
 
 });
